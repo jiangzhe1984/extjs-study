1ad428dbfce9d36203b4a22a8e8385ea26b79f2e
jiangzhe <jiangzhe1984@163.com>
2015-11-09 17:03:25 +0800

    20151109

--- app/view/main/List.js
+++ app/view/main/List.js
@@ -6,7 +6,8 @@ Ext.define('TutorialApp.view.main.List', {
     extend: 'Ext.grid.Panel',
     xtype: 'mainlist',
     requires: [
-        'TutorialApp.store.Personnel'
+        'TutorialApp.store.Personnel',
+        'TutorialApp.view.login.Login'
     ],
     columnLines: true,
     initComponent:function(){
@@ -14,6 +15,7 @@ Ext.define('TutorialApp.view.main.List', {
         var store = Ext.create('TutorialApp.store.Personnel');//创建store实例
         this.store = store;
         me.columns = [
+
             {
                 text: 'Name',
                 flex: 1,
@@ -40,7 +42,69 @@ Ext.define('TutorialApp.view.main.List', {
                     text:'查看',
                     handler: function(btn) {
                         var rec = btn.getWidgetRecord();
-                        Ext.Msg.alert("Button clicked", "Hey! " + rec.get('name'));
+
+                        Ext.create('Ext.window.Window', {
+
+                            requires: [
+                                'Ext.form.Panel',
+                                'Ext.form.field.Text',
+                                'Ext.layout.container.Fit',
+                                'Ext.toolbar.Fill',
+                                'Ext.toolbar.TextItem'
+                            ],
+
+
+                            bodyPadding: 10,
+                            title: '查看',
+                            closable: true,
+                            autoShow: true,
+                            width: 350,
+                            layout: 'fit',
+
+                            items: {
+                                xtype: 'form',
+                                reference: 'form',
+                                bodyPadding: 10,
+                                defaultType: 'textfield',
+
+                                fieldDefaults: {
+                                    anchor: '100%',
+                                    labelAlign: 'right',
+                                    labelWidth: 40
+                                },
+                                items: [{
+                                    fieldLabel: 'Name',
+                                    name: 'name',
+                                    itemId: 'name',
+                                    allowBlank: false,
+                                    bind: rec.get('name')
+                                }, {
+                                    fieldLabel: 'Email',
+                                    name: 'email',
+                                    inputType: 'email',
+                                    allowBlank: false,
+                                    bind: rec.get('email')
+                                }, {
+                                    fieldLabel: 'Phone',
+                                    name: 'phone',
+                                    inputType: 'phone',
+                                    allowBlank: false,
+                                    bind: rec.get('phone')
+                                }]/*,
+                                buttons: [{
+                                    xtype: 'tbtext',
+                                    html: '账户不存在或者密码错误',
+                                    style: 'color:red',
+                                    hidden: true
+                                }, '->', {
+                                    text: '登录',
+                                    reference: 'login',
+                                    formBind: true,
+                                    handler: 'onLoginClick'
+                                }]*/
+                            }
+                        });
+
                     }
                 }
             }
